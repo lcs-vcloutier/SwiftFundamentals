2befc9105379a31353abe2503a1135bded5c5f46
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-23 10:19:27 -0400

    Finished day 1 of swift fundamentals, 100% on tests too

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -9,3 +9,41 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+str = "Goodbye"
+
+//strings and integers
+var age = 38
+var population = 8_000_000
+
+//multi line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//doubles and booleans
+var pi = 3.141
+var awesome = true
+
+//string interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+//constants
+let taylor = "swift"
+
+//type annotations
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
