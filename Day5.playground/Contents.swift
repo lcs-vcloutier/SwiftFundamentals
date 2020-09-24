00606ccd312ecf46a6746d57b5630f2a0a0375f8
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 09:03:19 -0400

    day 5 done 100% on tests

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -6,6 +6,8 @@ Remember to take the practice tests for each topic.
 \
 Please [commit your work frequently](https://www.russellgordon.ca/cs/source-control/introduction/using-source-control.pdf) and document your progress once each day in [Spaces](http://ca.spacesedu.com).
  */
+//dont entirely understand parameter labels and omitting
+//sometimes define a function twice but use this as a reference
 
 import Cocoa
 
@@ -61,11 +63,36 @@ func greet2(_ person: String, nicely: Bool = true) {
 greet2("Taylor")
 greet2("Taylor", nicely: false)
 //variadic functions
+print("Haters", "gonna", "hate")
+func square2(numbers: Int...) {
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+square2(numbers: 1, 2, 3, 4, 5)
 
 //writing throwing functions
+enum PasswordError: Error {
+    case obvious
+}
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
 
+    return true
+}
 //running throwing functions
-
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
 //inout parameters
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
 
-
+var myNum = 10
+doubleInPlace(number: &myNum)
