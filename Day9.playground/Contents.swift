4aea2c064475f34d24a8231b961fed046019a620
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 12:20:24 -0400

    day 9 done 100% on all the tests

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -10,3 +10,68 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//initializers
+struct User {
+    var username: String
+
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+var user = User()
+user.username = "twostraws"
+
+//refering to the current instance
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+//lazy properties
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+struct Person2 {
+    var name: String
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person2(name: "Ed")
+
+//Static properties and methods
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+let edo = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+print(Student.classSize)
+
+//access control - weird
+struct Person3 {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
