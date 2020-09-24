2b5ac6b850bb778279229915e7552ef2e27fe9a8
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 09:52:26 -0400

    day 6 done, 100% on all the tests too

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -10,3 +10,35 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//creating basic closures
+let driving = {
+    print("I'm driving in my car")
+}
+driving()
+
+//accepting parameters in a closure
+let driving2 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+driving2("London")
+
+//returning values from a closure
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+let message = drivingWithReturn("London")
+print(message)
+
+//closures as parameters
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
+
+//trailing closure syntax - get used to this weirdness notice no brackets
+travel {
+    print("I'm driving in my car")
+}
