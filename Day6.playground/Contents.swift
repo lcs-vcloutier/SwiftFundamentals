e7fac94b323a9a0f8e90a5b5857b5981a2682d00
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 11:17:25 -0400

    Finished day 6, 100% on tests

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -6,7 +6,7 @@ Remember to take the practice tests for each topic.
 \
 Please [commit your work frequently](https://www.russellgordon.ca/cs/source-control/introduction/using-source-control.pdf) and document your progress once each day in [Spaces](http://ca.spacesedu.com).
  */
-
+//why use closures?
 import Cocoa
 
 var str = "Hello, playground"
