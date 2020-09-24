ca426ee90ed626ff4ba78f948b0ee032a0e55465
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 12:54:48 -0400

    day 10 done and 100% on all tests

--- Day10.playground/Contents.swift
+++ Day10.playground/Contents.swift
@@ -10,3 +10,87 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//creating your own classes
+//must always initialize = init
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+//class inheritance - subclass
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+//overriding methods
+class Dog2 {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+class Poodle2: Dog2 {
+    override func makeNoise() {
+        print("Yip!")
+    }
+}
+
+
+//final classes
+final class Dog3 {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+//copying objects
+class Singer {
+    var name = "Taylor Swift"
+}
+var singer = Singer()
+print(singer.name)
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+print(singer.name)
+
+//deinitializers
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive!")
+    }
+
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+        
+    }
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+//mutability
+
+class Singer3 {
+    var name = "Taylor Swift"
+}
+
+let taylor = Singer3()
+taylor.name = "Ed Sheeran"
+print(taylor.name)
