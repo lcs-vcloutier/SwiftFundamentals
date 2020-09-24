24dd025237898975e10beadd2f31ea74d2d54127
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 18:53:20 -0400

    Finished day 12, 100% on all the tests too

--- Day12.playground/Contents.swift
+++ Day12.playground/Contents.swift
@@ -12,22 +12,107 @@ import Cocoa
 var str = "Hello, playground"
 
 //handling missing data
+var age: Int? = nil
+age = 38
 
 //unwrapping optionals
+var name: String? = nil
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
 
 //unwrapping with guard
+func greet(_ name: String?) {
+    guard let unwrapped = name else {
+        print("You didn't provide a name!")
+        return
+    }
 
-//force unwrapping
+    print("Hello, \(unwrapped)!")
+}
+
+//force unwrapping --- only when your sure its safe
+let str2 = "5"
+let num = Int(str2)!
 
 //implicitly unwrapped optionals
+let age2: Int! = nil
+
 
 //nil coalescing
+func username(for id: Int) -> String? {
+    if id == 1 {
+        return "Taylor Swift"
+    } else {
+        return nil
+    }
+}
+let user = username(for: 15) ?? "Anonymous"
 
 //optional chaining
+let names = ["John", "Paul", "George", "Ringo"]
+let beatle = names.first?.uppercased()
+
 
 //optional try
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+if let result = try? checkPassword("password") {
+    print("Result was \(result)")
+} else {
+    print("D'oh.")
+}
+
+//below is a dangerous version of above
+//try! checkPassword("sekrit")
+//print("OK!")
 
 //failable initializers
+struct Person {
+    var id: String
+
+    init?(id: String) {
+        if id.count == 9 {
+            self.id = id
+        } else {
+            return nil
+        }
+    }
+}
 
 //typecasting
+class Animal { }
+class Fish: Animal { }
 
+class Dog: Animal {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+let pets = [Fish(), Dog(), Fish(), Dog()]
+
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
+}
