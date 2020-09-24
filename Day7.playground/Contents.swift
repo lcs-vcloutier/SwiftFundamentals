d369735d7a14fe27e47575d0067f1625b553aa4b
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 10:57:57 -0400

    day 7 started

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -10,3 +10,36 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+//using closures as parameters when they accept parameters
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+//using closures as parameters when they return values
+func travel2(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+travel2 { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+//shorthand parameter names - this is weird
+travel {
+    "I'm going to \($0) in my car"
+}
+//closures with multiple parameters
+
+//returning closures from functions
+
+//capturing values
+
+
