d79cae105e802cdcbf360e20184e73f15b7c581f
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-24 11:17:06 -0400

    finished day 7 100% on tests

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -6,7 +6,7 @@ Remember to take the practice tests for each topic.
 \
 Please [commit your work frequently](https://www.russellgordon.ca/cs/source-control/introduction/using-source-control.pdf) and document your progress once each day in [Spaces](http://ca.spacesedu.com).
  */
-
+//why use closures?
 import Cocoa
 
 var str = "Hello, playground"
@@ -37,9 +37,37 @@ travel {
     "I'm going to \($0) in my car"
 }
 //closures with multiple parameters
-
+func travel3(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+travel3 {
+    "I'm going to \($0) at \($1) miles per hour."
+}
 //returning closures from functions
+func travel4() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+let result = travel4()
+result("London")
 
 //capturing values
+let result2 = travel5()
+result("London")
 
+func travel5() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+result("London")
+result("London")
+result("London")
 
